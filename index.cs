15304dfbb2d077a69d0f9f9f422cd5d7ee74daba
adnanfathoni88 <katmanfatahoni@gmail.com>
2025-03-11 13:45:28 +0700

    fetch: penambahan dan pengurangan

--- index.cs
+++ index.cs
@@ -2,6 +2,20 @@ class Calculator
 {
     static void Main(String[] args)
     {
-        // Tugas 1
+        int a = 10;
+        int b = 6;
+
+        Console.WriteLine("Penambahan {0} + {1} = {2}", a, b, Penambahan(a));
+        Console.WriteLine("Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a));
+    }
+
+    static int Penambahan(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int Pengurangan(int a, int b)
+    {
+        return a - b;
     }
 }
